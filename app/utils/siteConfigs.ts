3daada116cd208a32f508f07ce7827f585e04814
Johnlin <johno1001741060168@gmail.com>
2026-06-10 21:01:41 +0800

    v2.4.0 release: 版本號升至 2.4.0

--- app/utils/siteConfigs.ts
+++ app/utils/siteConfigs.ts
@@ -1,5 +1,5 @@
 export const SITE_CONFIG = {
-  version: '2.4 Beta 5',
+  version: '2.4.0',
   name: '中臺機器人研究社',
   shortName: '中臺機器人研究社',
   url: process.env.NEXT_PUBLIC_SITE_URL || 'https://robotctust.com',
